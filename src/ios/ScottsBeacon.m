a5dcae478f9e5d7ffb08db70498e82df3a9d1554
Peter Nilsson <pni@ciklum.com>
2019-01-25 15:49:53 -0800

    Fix iOS build errors.

--- src/ios/ScottsBeacon.m
+++ src/ios/ScottsBeacon.m
@@ -8,20 +8,20 @@
 
 - (void)pluginInitialize
 {
-    NSLog("pluginInitialize");
+    NSLog(@"pluginInitialize");
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
 
     [self.locationManager requestAlwaysAuthorization];
     
-    NSString *identifier = "water_low";
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:"58A78BF8-E280-48A4-8668-B8D8CF947CF8"];
+    NSString *identifier = @"water_low";
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"58A78BF8-E280-48A4-8668-B8D8CF947CF8"];
     double major = 1;
     double minor = 64;
     
     CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid major:major minor:minor identifier:identifier];
 
-    [self.locationManager.startMonitoringForRegion:region];
+    [self.locationManager startMonitoringForRegion:region];
 }
 
 - (void)initializeScottsBeacon:(CDVInvokedUrlCommand*)command
@@ -34,30 +34,30 @@
 # pragma mark CLLocationManagerDelegate
 
 - (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region {
-    NSLog("didDetermineState: %@ %@", state, region);
+    NSLog(@"didDetermineState: %i %@", state, region);
 }
 
 -(void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
-    NSLog("didEnterRegion: %@", region);
+    NSLog(@"didEnterRegion: %@", region);
 }
 
 -(void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
-    NSLog("didExitRegion: %@", region);
+    NSLog(@"didExitRegion: %@", region);
 }
 
 - (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region {
-    NSLog("didStartMonitoringForRegion: %@", region);
+    NSLog(@"didStartMonitoringForRegion: %@", region);
 }
 
 - (void)locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error {
-    NSLog("monitoringDidFailForRegion: %@", region);
+    NSLog(@"monitoringDidFailForRegion: %@", region);
 }
 
 - (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
-    NSLog("didRangeBeacons: %@", region);
+    NSLog(@"didRangeBeacons: %@", region);
 
     for (CLBeacon* beacon in beacons) {
-        NSLog("didRangeBeacons: %@", beacon);
+        NSLog(@"didRangeBeacons: %@", beacon);
     }
 }
 
