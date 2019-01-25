860e734360af36c85dcfac0ab7af6d1994253c93
Peter Nilsson <pni@ciklum.com>
2019-01-25 12:18:05 -0800

    Fix a few mistakes.

--- src/android/ScottsBeacon.java
+++ src/android/ScottsBeacon.java
@@ -1,7 +1,6 @@
 package com.scotts.cordova.beacon;
 
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 
 import java.lang.Exception;
@@ -10,7 +9,6 @@ import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Identifier;
-import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
@@ -22,7 +20,7 @@ import org.apache.cordova.CordovaWebView;
 
 public class ScottsBeacon extends CordovaPlugin implements BootstrapNotifier {
 
-    public static final String TAG = "com.scotts.beacon";
+    public static final String TAG = ScottsBeacon.class.getSimpleName();
 
     private static Context sApplicationContext;
     private static RegionBootstrap sRegionBootstrap;
@@ -47,16 +45,16 @@ public class ScottsBeacon extends CordovaPlugin implements BootstrapNotifier {
 
         sApplicationContext = cordova.getActivity().getApplicationContext();
 
-        mBeaconManager = BeaconManager.getInstanceForApplication(context);
+        mBeaconManager = BeaconManager.getInstanceForApplication(sApplicationContext);
         mBeaconManager.getBeaconParsers().add(
                 new BeaconParser().setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24")
         );
 
-        sRegionBootstrap = new RegionBootstrap(context,
+        sRegionBootstrap = new RegionBootstrap(this,
                 new Region("water_low",
-                        "58A78BF8-E280-48A4-8668-B8D8CF947CF8",
-                        "1",
-                        "64")
+                        Identifier.parse("58A78BF8-E280-48A4-8668-B8D8CF947CF8"),
+                        Identifier.parse("1"),
+                        Identifier.parse("64"))
         );
     }
 
@@ -72,28 +70,11 @@ public class ScottsBeacon extends CordovaPlugin implements BootstrapNotifier {
     @Override
     public void didEnterRegion(Region region) {
         Log.d(TAG, "didEnterRegion: " + (region == null ? "-" : region.toString()));
-        // This call to disable will make it so the activity below only gets launched the first time a beacon is seen (until the next time the app is launched)
-        // if you want the Activity to launch every single time beacons come into view, remove this call.
         //sRegionBootstrap.disable();
-        Intent intent = new Intent(this, MainActivity.class);
-        // IMPORTANT: in the AndroidManifest.xml definition of this activity, you must set android:launchMode="singleInstance" or you will get two instances
-        // created when a user launches the activity manually and it gets launched from here.
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        this.startActivity(intent);
     }
 
     @Override
     public void didExitRegion(Region region) {
         Log.d(TAG, "didExitRegion: " + (region == null ? "-" : region.toString()));
-    }
-
-    @Override
-    public void didRangeBeaconsInRegion(java.util.Collection<Beacon> beacons, Region region) {
-        Log.d(TAG, "didRangeBeaconsInRegion: " + (region == null ? "-" : region.toString()));
-        if (beacons != null) {
-            for (Beacon beacon : beacons) {
-                Log.d(TAG, "beacon: " + beacon.toString());
-            }
-        }
     }
 }
