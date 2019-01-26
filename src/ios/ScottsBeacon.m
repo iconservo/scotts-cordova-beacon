d9e640c51db3d1989dca3610945676dd6d2c9591
Peter Nilsson <pni@ciklum.com>
2019-01-25 23:19:54 -0800

    Add ranging and initial request for region state.

--- src/ios/ScottsBeacon.m
+++ src/ios/ScottsBeacon.m
@@ -14,12 +14,10 @@
 
     [self.locationManager requestAlwaysAuthorization];
     
-    NSString *identifier = @"water_low";
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"58A78BF8-E280-48A4-8668-B8D8CF947CF8"];
-    double major = 1;
-    double minor = 64;
-    
-    CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid major:major minor:minor identifier:identifier];
+    NSString *identifier = @"mg12beacon";
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"58a78bf8-e280-48a4-8668-b8d8cf947cf8"];
+
+    CLBeaconRegion *region = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:identifier];
 
     [self.locationManager startMonitoringForRegion:region];
 }
@@ -35,18 +33,28 @@
 
 - (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region {
     NSLog(@"didDetermineState: %i %@", state, region);
+
+    if ([region isKindOfClass:[CLBeaconRegion class]] && state == CLRegionStateInside) {
+        [self locationManager:manager didEnterRegion:region];
+    }
 }
 
 -(void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region {
     NSLog(@"didEnterRegion: %@", region);
+
+    [self.locationManager startRangingBeaconsInRegion:region];
 }
 
 -(void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region {
     NSLog(@"didExitRegion: %@", region);
+
+    [self.locationManager stopRangingBeaconsInRegion:region];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region {
     NSLog(@"didStartMonitoringForRegion: %@", region);
+
+    [manager requestStateForRegion:region];
 }
 
 - (void)locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error {
@@ -57,7 +65,7 @@
     NSLog(@"didRangeBeacons: %@", region);
 
     for (CLBeacon* beacon in beacons) {
-        NSLog(@"didRangeBeacons: %@", beacon);
+        NSLog(@"didRangeBeacons: beacon: %@", beacon);
     }
 }
 
